cdf88936fb1aa98eec294903c12cd4278a503ba3
Everton <everton.nnrb@gmail.com>
2023-06-12 13:29:15 -0400

    start

---
+++ src/main/java/br/com/payroll/App.java
@@ -0,0 +1,7 @@
+package br.com.payroll;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
---
+++ src/main/java/br/com/payroll/entities/Person.java
@@ -0,0 +1,10 @@
+package br.com.payroll.entities;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String email;
+
+}
